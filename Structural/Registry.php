81a0ba873045ce1db2b81c00d4ee176214a260a5
artem_kozyr <artem.kozyr777@gmail.com>
2023-06-04 17:49:27 +0300

    add Registry.php fix folder

---
+++ Structural/Registry.php
@@ -0,0 +1,30 @@
+<?php
+
+abstract class Registry
+{
+    private static array $services = [];
+    final public static function setService($key, Service $service): void
+    {
+        self::$services[$key] = $service;
+    }
+    final public static function getService($key): ?Service
+    {
+        if (isset(self::$services[$key]))   {
+            return self::$services[$key];
+        }
+        return null;
+    }
+}
+
+class  Service
+{
+
+}
+
+$service = new Service();
+
+Registry::setService(1, $service);
+
+$service = Registry::getService(1);
+
+var_dump($service);
